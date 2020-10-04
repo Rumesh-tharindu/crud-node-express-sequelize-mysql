cea6870723bcdae0c24fa7799a640b710f3b64eb
rumesh-tharindu <rumeshtharindu2020@gmail.com>
2020-10-04 16:43:05 +0530

    add body-parser and remove sequilize cli

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express=require('express')
+const cors=require('cors')
+const bodyParser=require('body-parser')
 
 const app=express()
+var corsOptions = {
+    origin: "http://localhost:8081"
+  };
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(cors(corsOptions))
 
 app.get('/',(req,res)=>{
     res.send('Hello')
 })
-
-app.listen('5000',()=>{
-    console.log("port 5000 is listenign")
+const PORT=process.env.PORT || 8080
+app.listen(PORT,()=>{
+    console.log(`Server is runnign on port ${PORT}`)
 })
